e988324b91b8c646072a5573689b143dcd10c3d6
potatoyz908 <euller2005@gmail.com>
2024-12-17 08:49:01 -0300

    refactor: reorganiza as classes com os packages apropriados

--- src/main/java/com/repository/FilmeRepository.java
+++ src/main/java/com/repository/FilmeRepository.java
@@ -1,7 +1,9 @@
-package com.entidade;
+package com.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.entidade.Filme;
 
 @Repository
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
--- src/main/java/com/service/FilmeService.java
+++ src/main/java/com/service/FilmeService.java
@@ -1,8 +1,11 @@
-package com.entidade;
+package com.service;
 
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.entidade.Filme;
+import com.repository.FilmeRepository;
 
 @Service
 public class FilmeService {
